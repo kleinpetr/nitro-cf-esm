cd617b4fa93b6c813ad8b9ef815f12ebbcf23328
Klein Petr <petr@kleinpetr.com>
2025-03-12 13:29:37 +0100

    chore: init

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,5 +1,8 @@
 //https://nitro.unjs.io/config
 export default defineNitroConfig({
   srcDir: "server",
-  compatibilityDate: "2025-03-12"
+  compatibilityDate: "2025-03-12",
+  unenv: {
+    external: ["cloudflare:workers"]
+  }
 });
